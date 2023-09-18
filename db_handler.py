ed6a27cae27a5fcbe661ad4c3d0ae702bdd9f5a0
Louie Constable <Lzc5535@psu.edu>
2023-09-18 11:49:27 -0400

    finished db handler

---
+++ db_handler.py
@@ -0,0 +1,64 @@
+import sqlalchemy
+import pandas as pd
+
+class DB_conn:
+
+    def __init__(self, dbtype, driver  , Server , Database , Trusted_Connection , Username =None , Password =None):
+
+            self.driver = driver
+            self.Server = Server
+            self.database = Database
+            self.Trusted_Connection = Trusted_Connection
+            self.username = Username
+            self.Password = Password 
+            self.dbtype = dbtype
+
+
+class MsSQL(DB_conn):
+
+
+    def __init__(self,dbtype , driver, Server , Database , Trusted_Connection , Username = None, Password =None ):
+        super().__init__(dbtype , driver, Server, Database , Trusted_Connection, Username , Password )
+
+        self.con = sqlalchemy.create_engine('mssql+pyodbc://@' + f'{self.Server}' + '/' + f'{self.database}' + '?trusted_connection=yes&driver=ODBC Driver 17 for SQL Server')
+
+        
+    def query(self,query):
+            result  = pd.read_sql(query, con= self.con)
+            return result
+
+    def close(self):
+            pass
+      
+        
+
+    
+
+
+
+
+def main():
+    Driver="Louis"
+    Server="LOUIS-PC"
+    Database="Crypto_market_prices"
+    Trusted_Connection="yes"
+    
+
+
+    mssql = MsSQL(driver=Driver, Server=Server, Database=Database,Trusted_Connection=Trusted_Connection, dbtype="MSSQL")
+
+    
+    
+    print(mssql.query('Select * from symbols'))
+
+
+     
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
