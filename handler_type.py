d97775a299eebb65fdc4ccbcdc5f2027d4645f8f
Louie Constable <Lzc5535@psu.edu>
2023-09-18 16:08:28 -0400

    finished handler enum

---
+++ handler_type.py
@@ -0,0 +1,23 @@
+
+from enum import Enum
+class HandlerType(Enum):
+    SFTP = 'sftp'
+    FTP = 'sftp'
+    FILE = 'file'
+    RSYNC = 'rsync'
+    TELNET = 'telnet'
+
+
+    @classmethod 
+    def has_value(cls, value):
+        return cls._value2member_map_
+
+
+    @classmethod
+    def has_member(cls, value):
+        return value in cls._member_names_
+    
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+    
